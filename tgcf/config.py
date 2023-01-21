dc0346f2d111772c9584082a691a22b58744c141
KarthikTamil7 <108778191+KarthikTamil7@users.noreply.github.com>
2023-01-21 16:58:26 +0530

    Update config.py

--- tgcf/config.py
+++ tgcf/config.py
@@ -78,12 +78,8 @@ class BotMessages(BaseModel):
 
     See help for more Details.
 
-    Maintained By : [Karthik](https://t.me/HMTD_Karthik)**""".replace(
-        "    ", ""
-    )
-    bot_help: str = """"**The Ultimate Tool to Automate Custom Telegram Message Forwarding. Live Syncer, Auto Poster, Backup Bot, Cloner, Chat Forwarder, Duplicator, ... Call it whatever you like! tgcf can fulfill Your Custom Needs.**""".replace(
-        "    ", ""
-    )
+    Maintained By : [Karthik](https://t.me/HMTD_Karthik)**"""
+    bot_help: str = """"**The Ultimate Tool to Automate Custom Telegram Message Forwarding. Live Syncer, Auto Poster, Backup Bot, Cloner, Chat Forwarder, Duplicator, ... Call it whatever you like! tgcf can fulfill Your Custom Needs.**"""
     bot_about: str = """**🤖 My Name : [UK Auto Forward Bot](https://t.me/UK_Auto_Forward_Bot)
 
     🧑🏻‍💻 Developer : [Karthik](https://t.me/HMTD_Karthik)
@@ -96,9 +92,7 @@ class BotMessages(BaseModel):
 
     📢 Updates Channel : [UK Movies Zone (Updates)](https://t.me/UK_Movies_Zone_Updates)
 
-    🌐 Website : [www.HMTDMovies.tk](https://www.HMTDMovies.tk)**""".replace(
-        "    ", ""
-    )
+    🌐 Website : [www.HMTDMovies.tk](https://www.HMTDMovies.tk)**"""
 
 class Config(BaseModel):
     """The blueprint for tgcf's whole config."""
